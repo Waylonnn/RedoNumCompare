7a02932d62a3a0b3a2befe1671c2873bc5fad9c7
Waylon! <weese.619547@swl.k12.oh.us>
2024-03-15 00:27:10 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        int num2;
+        System.out.println("Enter Two Numbers: ");
+            if (scan.hasNextInt()) {
+                num1 = scan.nextInt();
+                if(scan.hasNextInt()){
+                    num2 = scan.nextInt();
+                    if(num1 > num2){
+                        System.out.println(num1 + " is greater than " + num2);
+                    } else if (num1 == num2){
+                        System.out.println(num1 + " is equal to " + num2);
+                    } else {
+                        System.out.println(num2 + " is greater than " + num1);
+                    }
+                } else System.out.println("Error: Not an integer");
+            } else System.out.println("Error: Not an integer");
+
+
     }
 }
+/*Write a program that takes two numbers as inputs from the user and compares them.
+    The program should indicate which of the two numbers is bigger.
+    EX: 5 is greater than 4
+    EX: 3 is equal to 3
+    Ensure that the user is typing invalid inputs. If they don't, an error message should be displayed and the program should stop.
+    Test Runs:
+    3 and 4
+    5 and 5
+    6 and 2
+*/
